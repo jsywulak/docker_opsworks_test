a7170a59840ba8dc9fca06babe78d3d28eb62de3
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-05-06 18:33:07 -0500

    making more jenkins specific

--- jonny_docker/recipes/pull_images.rb
+++ jonny_docker/recipes/pull_images.rb
@@ -1,10 +1,9 @@
 # Pull each of our defined apps
-node[:images].each do |name, image|  
-  script "pull_app_#{name}_image" do
-    interpreter "bash"
-    user "root"
-    code <<-EOH
-      docker pull #{image}
-    EOH
-  end
+
+script "pull_app_#{name}_image" do
+  interpreter "bash"
+  user "root"
+  code <<-EOH
+    docker pull #{node[:jenkins][:image]}
+  EOH
 end
--- jonny_docker/recipes/run_containers.rb
+++ jonny_docker/recipes/run_containers.rb
@@ -1,11 +1,7 @@
-# Run each app. We don't expose any ports since Nginx will handle all incoming traffic as a proxy
-node[:my_apps].each do |name, image|  
-  script "run_app_#{name}_container" do
-    interpreter "bash"
-    user "root"
-    code <<-EOH
-      docker run -d --name=#{name} #{image}
-    EOH
-  end
+script "run_jenkins_container" do
+  interpreter "bash"
+  user "root"
+  code <<-EOH
+    docker run -d -p 80:80 --name=jenkins #{node[:jenkins][:image]}
+  EOH
 end
-
