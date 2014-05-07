c9cf578c7cbacbd4db0b2737eeb695ef4da4cf0a
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-05-07 00:15:09 -0500

    jenkins runs on port 8080, but the SGs are on port 80, so wire those together.

--- jonny_docker/recipes/run_containers.rb
+++ jonny_docker/recipes/run_containers.rb
@@ -2,6 +2,6 @@ script "run_jenkins_container" do
   interpreter "bash"
   user "root"
   code <<-EOH
-    docker run -d -p 80:80 --name=jenkins #{node[:docker][:jenkins]}
+    docker run -d -p 80:8080 --name=jenkins #{node[:docker][:jenkins]}
   EOH
 end
